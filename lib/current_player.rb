410b376887b0c8f40ea643e0bd8223dd661cf19e
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-15 05:34:59 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
-def turn_count(board) 
+def turn_count(board)
   counter = 0
   board.each do |counter|
     if counter == "X" || counter =="O"
       counter += 1
     end
-    return counter 
+    return counter
 end
 end 
